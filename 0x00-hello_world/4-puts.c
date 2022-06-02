1791b64381dfb4f1b1ca2091f8f8f0fe648c2236
FataiKofT <kareemfatai158@gmail.com>
2022-06-02 14:43:13 -0700

    puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - This is a description
+*
+* Return: 0
+*/
+int main(void)
+{
+char str1[] = "\"Programming is like building a multilingual puzzle";
+
+puts(str1);
+return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+My readme on alx low level programming
