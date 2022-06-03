4ddb0d4f69b387c012d4e65a9405ea48078ba3e1
FataiKofT <kareemfatai158@gmail.com>
2022-06-02 18:09:20 -0700

    C program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdin.h>
+
+/**
+ * main - entry point
+ * description - C program that prints using write function
+ * Return: 1
+*/
+
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
+return (0);
+}
